d2dd6ebff68ab1db51bdd689e9a0a9be795df98b
Zanon Fabio <fabio.zanon@blnet.lokal>
2019-04-09 13:58:38 +0200

    Zwischenstand

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 
 window.onscroll = function() {scrollFunction()};
 
+
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("myBtn").style.display = "block";
@@ -22,3 +23,32 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+
+
+
+
+
+
+
+//Bildanzeige
+
+$(document).ready(function() {
+  $("#unten img").on("click", function() {
+    var src = $(this).attr("src");
+    var alt = $(this).attr("alt");
+        $("#unten img").css("opacity", "1");
+        $(this).css("opacity", "0.3");
+        $("#oben img").fadeOut(400);
+        setTimeout(function(){
+        $("#oben img")
+        .removeAttr("src")
+        .removeAttr("alt")
+        .attr("src", src)
+        .attr("alt", alt)
+        .fadeIn(400);
+}, 400);
+    });
+});
